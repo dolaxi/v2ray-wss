9d8b60358df088f3c3405ef6dec2e614426c69a7
dolaxi <daxilow@outlook.com>
2026-01-23 11:24:59 +0800

    Update script URLs to new repository

--- tcp-wss.sh
+++ tcp-wss.sh
@@ -177,19 +177,19 @@ EOF
 }
 
 install_ssrust(){
-    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/ss-rust.sh && bash ss-rust.sh
+    wget https://raw.githubusercontent.com/dolaxi/v2ray-wss/main/ss-rust.sh && bash ss-rust.sh
 }
 
 install_reality(){
-    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/reality.sh && bash reality.sh
+    wget https://raw.githubusercontent.com/dolaxi/v2ray-wss/main/reality.sh && bash reality.sh
 }
 
 install_hy2(){
-    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/hy2.sh && bash hy2.sh
+    wget https://raw.githubusercontent.com/dolaxi/v2ray-wss/main/hy2.sh && bash hy2.sh
 }
 
 install_https(){
-    wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/https.sh && bash https.sh
+    wget https://raw.githubusercontent.com/dolaxi/v2ray-wss/main/https.sh && bash https.sh
 }
 
 client_v2ray(){
